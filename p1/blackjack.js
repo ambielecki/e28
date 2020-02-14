a51d7c9c325c89db64672b06e0295ea943b1690e
Andrew Bielecki <andrew.bielecki@healthcaresource.com>
2020-02-14 13:16:13 -0500

    week-3 :: p1 playing with Bulma

--- p1/blackjack.js
+++ p1/blackjack.js
@@ -1,7 +1,22 @@
+Vue.component('playing-card', {
+    template: `
+        <div class="column is-narrow card_wrapper">
+            <div class="card playing_card show_card">
+    
+            </div>
+        </div>
+    `,
+    props: ['is_dealer_turn', 'card'],
+});
+
+
 let blackjack = new Vue({
     el: '#blackjack',
     data: {
-
+        dealer_hand: [],
+        deck: [],
+        is_dealer_turn: false,
+        player_hand: [],
     },
     methods: {
 
