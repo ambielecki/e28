0aa23271e5bf543684cc04fdc2164bb59af9f806
Andrew Bielecki <andrew.bielecki@healthcaresource.com>
2020-02-14 09:49:35 -0500

    week-3 :: state after intro

--- intro/script.js
+++ intro/script.js
@@ -2,5 +2,13 @@ let app = new Vue({
     el: '#app',
     data: {
         player_name: '',
+        guesses: [],
+        guess: null,
+    },
+    methods: {
+        handleGuess: function () {
+            this.guesses.push(this.guess);
+            this.guess = null;
+        },
     },
 });
