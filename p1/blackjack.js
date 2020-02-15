d6d8953ccf68fe658d149c314f5e90b40c6ad805
Andrew Bielecki <ambielecki@gmail.com>
2020-02-15 08:15:32 -0500

    week 3 :: messages for p1

--- p1/blackjack.js
+++ p1/blackjack.js
@@ -22,6 +22,14 @@ const suits = {
 };
 
 const number_of_decks = 6;
+const minimum_bet = 100;
+
+const text_danger = 'has-text-danger';
+const text_success = 'has-text-success';
+
+function getDefaultData() {
+
+}
 
 Vue.component('playing-area', {
     template: `
@@ -31,7 +39,11 @@ Vue.component('playing-area', {
                     <h2 class="subtitle has-text-white">{{ owner }}</h2>
                 </div>
                 
-                <playing-card v-for="card in cards" v-bind:card="card"></playing-card>
+                <playing-card 
+                  v-for="(card, index) in cards" 
+                  v-bind:card="card"
+                  v-bind:key="index"
+                ></playing-card>
             </div>
         </div>
     `,
@@ -59,40 +71,57 @@ let blackjack = new Vue({
     el: '#blackjack',
 
     data: {
+        current_bet: null,
         dealer_hand: [],
         deck: [],
+        initial_bet: minimum_bet,
         is_dealer_turn: false,
+        message: null,
+        message_class: null,
+        minimum_bet: minimum_bet,
         player_hand: [],
+        player_purse: 5000,
+        scoreboard: {
+            draws: 0,
+            losses: 0,
+            wins: 0,
+        },
+        winner: null,
     },
 
     computed: {
         dealer_status: function () {
-            return this.calcuateStatus(this.dealer_hand);
+            return this.calculateStatus(this.dealer_hand);
         },
 
         player_status: function () {
-            return this.calcuateStatus(this.player_hand);
+            return this.calculateStatus(this.player_hand);
         }
     },
 
     methods: {
-        calcuateStatus(hand) {
+        calculateStatus(hand) {
             let value = 0;
-            let number_of_aces = 0;
+            let has_ace = false;
 
             hand.forEach(card => {
                 if (card.text === 'A') {
-                    number_of_aces++;
+                    has_ace = true;
                 }
 
                 value += card.value;
             });
 
-            if (number_of_aces > 0 && value + 10 <= 21) {
+            if (has_ace && value + 10 <= 21) {
                 return {value: value + 10, is_soft: true};
             }
 
             return {value: value, is_soft: false};
+        },
+
+        clearMessage() {
+            this.message = null;
+            this.message_class = null;
         },
 
         dealCard(show_card = true) {
@@ -100,6 +129,28 @@ let blackjack = new Vue({
             card.show_card = show_card;
 
             return card;
+        },
+
+        decreaseBet() {
+            this.clearMessage();
+
+            if (this.initial_bet - minimum_bet >= minimum_bet) {
+                this.initial_bet -= minimum_bet;
+            } else {
+                this.message = 'You cannot bet below the minimum bet';
+                this.message_class = text_danger;
+            }
+        },
+
+        increaseBet() {
+            this.clearMessage();
+
+            if (this.initial_bet + minimum_bet <= this.player_purse) {
+                this.initial_bet += minimum_bet;
+            } else {
+                this.message = 'You do not have enough money left to bet more';
+                this.message_class = text_danger;
+            }
         },
 
         shuffleDeck() {
