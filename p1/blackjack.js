9e895dfa99e3711ad492b990b1016ea5d2991cd9
Andrew Bielecki <ambielecki@gmail.com>
2020-02-16 11:00:46 -0500

    week 3 :: mostly functional, need to handle player blackjack

--- p1/blackjack.js
+++ p1/blackjack.js
@@ -26,54 +26,28 @@ const minimum_bet = 100;
 
 const text_danger = 'has-text-danger';
 const text_success = 'has-text-success';
+const text_black = 'has-text-black';
+
+const end_messages = {
+    dealer_blackjack: 'You lost, dealer had blackjack',
+    player_blackjack: 'Blackjack! You win 1.5x your bet',
+    win: 'You won!',
+    lose: 'Sorry, you lost',
+    bust: 'You busted and lost your bet',
+    push: 'You pushed, your bet has been returned',
+};
+
+const winner_dealer = 'dealer';
+const winner_player = 'player';
+const winner_push = 'push';
 
 function getDefaultData() {
-
-}
-
-Vue.component('playing-area', {
-    template: `
-        <div class="column is-full" id="dealer_area">
-            <div class="columns is-mobile is-multiline">
-                <div class="column is-full">
-                    <h2 class="subtitle has-text-white">{{ owner }}</h2>
-                </div>
-                
-                <playing-card 
-                  v-for="(card, index) in cards" 
-                  v-bind:card="card"
-                  v-bind:key="index"
-                ></playing-card>
-            </div>
-        </div>
-    `,
-    props: ['owner', 'cards'],
-});
-
-Vue.component('playing-card', {
-    template: `
-        <div class="column level is-narrow card_wrapper">
-            <div 
-              class="card has-text-centered level-item playing_card" 
-              v-bind:class="[card.class, card.show_card ? 'show_card' : '']"
-            >
-                <div class="card-content">
-                    <p v-if="card.show_card">{{ card.text }}<span v-html="card.suit"></span></p>
-                </div>
-            </div>
-        </div>
-    `,
-    props: ['is_dealer_turn', 'card'],
-});
-
-
-let blackjack = new Vue({
-    el: '#blackjack',
-
-    data: {
+    return {
+        can_double: false,
         current_bet: null,
         dealer_hand: [],
         deck: [],
+        game_over: true,
         initial_bet: minimum_bet,
         is_dealer_turn: false,
         message: null,
@@ -87,7 +61,58 @@ let blackjack = new Vue({
             wins: 0,
         },
         winner: null,
-    },
+    };
+}
+
+Vue.component('playing-area', {
+    template: `
+        <div class="column is-full" id="dealer_area">
+            <div class="columns is-mobile is-multiline">
+                <div class="column is-full">
+                    <h2 class="subtitle has-text-white">{{ owner }} - {{ owner_score }}</h2>
+                </div>
+                
+                <playing-card 
+                  v-for="(card, index) in cards" 
+                  v-bind:card="card"
+                  v-bind:key="index"
+                ></playing-card>
+            </div>
+        </div>
+    `,
+    props: ['owner', 'owner_score', 'cards'],
+});
+
+Vue.component('playing-card', {
+    template: `
+        <div class="column level is-narrow card_wrapper">
+            <div 
+              class="card has-text-centered level-item playing_card" 
+              v-bind:class="[card.class, card.show_card ? '' : 'has-background-link']"
+            >
+                <div class="card-content">
+                    <p v-if="card.show_card">{{ card.text }}<span v-html="card.suit"></span></p>
+                </div>
+            </div>
+        </div>
+    `,
+    props: ['is_dealer_turn', 'card'],
+});
+
+Vue.component('message', {
+    template: `
+        <p v-bind:class="message_class" class="card-header-title">
+            {{ message }}
+        </p>
+    `,
+    props: ['message', 'message_class'],
+});
+
+
+let blackjack = new Vue({
+    el: '#blackjack',
+
+    data: getDefaultData(),
 
     computed: {
         dealer_status: function () {
@@ -96,7 +121,23 @@ let blackjack = new Vue({
 
         player_status: function () {
             return this.calculateStatus(this.player_hand);
-        }
+        },
+
+        allowed_to_double: function () {
+            return this.current_bet <= this.player_purse && this.can_double;
+        },
+
+        scoreboard_total: function () {
+            return this.scoreboard.wins + this.scoreboard.losses + this.scoreboard.draws;
+        },
+
+        dealer_score_view: function () {
+            return this.is_dealer_turn ? this.dealer_status.value : '?';
+        },
+
+        player_score_view: function () {
+            return this.player_status.is_soft ? 'Soft ' + this.player_status.value : this.player_status.value;
+        },
     },
 
     methods: {
@@ -117,6 +158,15 @@ let blackjack = new Vue({
             }
 
             return {value: value, is_soft: false};
+        },
+
+        clearGame() {
+            this.shuffleDeck();
+            this.dealer_hand = [];
+            this.player_hand = [];
+            this.can_double = true;
+            this.winner = null;
+            this.is_dealer_turn = false;
         },
 
         clearMessage() {
@@ -170,14 +220,109 @@ let blackjack = new Vue({
                 });
             }
         },
-    },
 
-    mounted() {
-        this.shuffleDeck();
+        startGame() {
+            this.clearMessage();
 
-        this.dealer_hand.push(this.dealCard());
-        this.player_hand.push(this.dealCard());
-        this.dealer_hand.push(this.dealCard(false));
-        this.player_hand.push(this.dealCard());
+            this.current_bet = this.initial_bet;
+            this.player_purse -= this.current_bet;
+
+            this.clearGame();
+
+            this.dealer_hand.push(this.dealCard());
+            this.player_hand.push(this.dealCard());
+            this.dealer_hand.push(this.dealCard(false));
+            this.player_hand.push(this.dealCard());
+
+            if (this.dealer_status.value === 21) {
+                this.is_dealer_turn = true;
+                this.showDealerCards();
+                this.endGame(winner_dealer, end_messages.dealer_blackjack);
+            }
+
+            this.game_over = false;
+        },
+
+        showDealerCards() {
+            this.is_dealer_turn = true;
+            this.dealer_hand.forEach(card => {
+                card.show_card = true
+            });
+        },
+
+        endGame(winner, message = '') {
+            this.game_over = true;
+            this.message = message;
+
+            switch(winner) {
+                case winner_player:
+                    this.scoreboard.wins++;
+                    this.player_purse += (2 * this.current_bet);
+                    break;
+                case winner_dealer:
+                    this.scoreboard.losses++;
+                    break;
+                default:
+                    this.scoreboard.draws++;
+                    this.player_purse += this.current_bet;
+            }
+
+            switch(message) {
+                case end_messages.dealer_blackjack:
+                case end_messages.lose:
+                case end_messages.bust:
+                    this.message_class = text_danger;
+                    break;
+                case end_messages.player_blackjack:
+                case end_messages.win:
+                    this.message_class = text_success;
+                    break;
+                default:
+                    this.message_class = text_black;
+            }
+        },
+
+        hitMe() {
+            this.can_double = false;
+            this.player_hand.push(this.dealCard());
+
+            if (this.player_status.value > 21) {
+                this.endGame(winner_dealer, end_messages.bust)
+            }
+        },
+
+        stay() {
+            this.can_double = false;
+            this.showDealerCards();
+
+            while (this.dealer_status.value < 17 || (this.dealer_status.value === 17 && this.dealer_status.value.is_soft)) {
+                this.dealer_hand.push(this.dealCard());
+            }
+
+            this.game_over = true;
+            if (this.dealer_status.value > 21 || this.player_status.value > this.dealer_status.value) {
+                this.endGame(winner_player, end_messages.win);
+            } else if (this.dealer_status.value > this.player_status.value) {
+                this.endGame(winner_dealer, end_messages.lose);
+            } else {
+                this.endGame(winner_push, end_messages.push);
+            }
+        },
+
+        doubleDown() {
+            this.player_purse -= this.current_bet;
+            this.current_bet *= 2;
+            this.player_hand.push(this.dealCard());
+
+            if (this.player_status.value > 21) {
+                this.endGame(winner_dealer, end_messages.bust)
+            } else {
+                this.stay();
+            }
+        },
+
+        resetStats() {
+            Object.assign(this.$data, getDefaultData());
+        },
     },
 });
