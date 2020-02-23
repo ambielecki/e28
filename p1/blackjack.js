85b1e9e0d158968e9b0754a09edd3b8956155df8
Andrew Bielecki <ambielecki@gmail.com>
2020-02-23 08:47:43 -0500

    week-4 :: HR instead of card for audit

--- p1/blackjack.js
+++ p1/blackjack.js
@@ -187,14 +187,13 @@ Vue.component('audit', {
             <div class="card-content">
                 <div class="columns is-multiline">
                     <div class="column is-full">
-                        <div v-for="(record, index) in audit" :key="index" class="card">
-                            <div class="card-content audit">
-                                <p><b>Round: {{ record.round }}</b></p>
-                                <p><b>Dealer:</b> {{ record.dealer_total }}  {{ record.dealer_hand }}</p>
-                                <p><b>Player:</b> {{ record.player_total }}  {{ record.player_hand }}</p>
-                                <p><b>Winner:</b> {{ record.winner }}</p>
-                                <p><b>Initial Bet:</b> {{ record.initial_bet }}, <b>Doubled:</b> {{ record.doubled }}, <b>Purse:</b> {{ record.purse_adjustment }}</p>
-                            </div>
+                        <div v-for="(record, index) in audit" :key="index" class="audit">
+                            <p><b>Round: {{ record.round }}</b></p>
+                            <p><b>Dealer:</b> {{ record.dealer_total }}  {{ record.dealer_hand }}</p>
+                            <p><b>Player:</b> {{ record.player_total }}  {{ record.player_hand }}</p>
+                            <p><b>Winner:</b> {{ record.winner }}</p>
+                            <p><b>Initial Bet:</b> {{ record.initial_bet }}, <b>Doubled:</b> {{ record.doubled }}, <b>Purse:</b> {{ record.purse_adjustment }}</p>
+                            <hr>
                         </div>
                     </div>
                 </div>
