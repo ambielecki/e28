59acb4b4ba24635b88c0beef776c46f15fbb25b0
Andrew Bielecki <andrew.bielecki@healthcaresource.com>
2020-02-28 14:08:13 -0500

    week-5 :: why make this complicated

--- word-scramble/scramble.js
+++ word-scramble/scramble.js
@@ -9,7 +9,36 @@ let app = new Vue({
         player_name: '',
         selected_word: '',
         selected_sport: '',
-        words: getWords(),
+        words: {
+            baseball: {
+                batter: 'The person holding the bat',
+                pitcher: 'Stands on the mound',
+                umpire: 'Calls balls and strikes',
+                fastball: 'Pitch with the highest speed',
+                strike: 'A swing and a miss',
+            },
+            football: {
+                touchdown: 'Worth six points',
+                interception: 'A pass thrown to the defense',
+                safety: 'Sacked in your own end zone',
+                fumble: 'Dropped the ball',
+                yard: 'Unit of distance',
+            },
+            basketball: {
+                foul: 'Contact with another player',
+                dribble: 'Bounce the ball up and down',
+                basket: 'Where you shoot the ball',
+                center: 'Generally your tallest player',
+                swish: 'The sound of a shot that only hits the net',
+            },
+            hockey: {
+                puck: 'Hockey does not have a ball, it has a ...',
+                goalie: 'Defends the goal',
+                check: 'Legal hit of another player',
+                zamboni: 'Fixes the ice',
+                canada: 'The NHL plays in the US and ...'
+            }
+        },
     },
     computed: {
         sports: function () {
@@ -56,37 +85,3 @@ let app = new Vue({
         },
     },
 });
-
-// just makes data a lot cleaner (and could get this from an api or such)
-function getWords() {
-    return {
-        baseball: {
-            batter: 'The person holding the bat',
-            pitcher: 'Stands on the mound',
-            umpire: 'Calls balls and strikes',
-            fastball: 'Pitch with the highest speed',
-            strike: 'A swing and a miss',
-        },
-        football: {
-            touchdown: 'Worth six points',
-            interception: 'A pass thrown to the defense',
-            safety: 'Sacked in your own end zone',
-            fumble: 'Dropped the ball',
-            yard: 'Unit of distance',
-        },
-        basketball: {
-            foul: 'Contact with another player',
-            dribble: 'Bounce the ball up and down',
-            basket: 'Where you shoot the ball',
-            center: 'Generally your tallest player',
-            swish: 'The sound of a shot that only hits the net',
-        },
-        hockey: {
-            puck: 'Hockey does not have a ball, it has a ...',
-            goalie: 'Defends the goal',
-            check: 'Legal hit of another player',
-            zamboni: 'Fixes the ice',
-            canada: 'The NHL plays in the US and ...'
-        }
-    }
-}
