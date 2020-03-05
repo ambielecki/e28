cf5846a39704edce4b9b198a63320298b623055d
Andrew Bielecki <andrew.bielecki@healthcaresource.com>
2020-03-05 10:54:28 -0500

    week-5 :: sports does not need to be computed

--- word-scramble/scramble.js
+++ word-scramble/scramble.js
@@ -1,3 +1,34 @@
+const words = {
+    baseball: {
+        batter: 'The person holding the bat',
+        pitcher: 'Stands on the mound',
+        umpire: 'Calls balls and strikes',
+        fastball: 'Pitch with the highest speed',
+        strike: 'A swing and a miss',
+    },
+    football: {
+        touchdown: 'Worth six points',
+        interception: 'A pass thrown to the defense',
+        safety: 'Sacked in your own end zone',
+        fumble: 'Dropped the ball',
+        yard: 'Unit of distance',
+    },
+    basketball: {
+        foul: 'Contact with another player',
+        dribble: 'Bounce the ball up and down',
+        basket: 'Where you shoot the ball',
+        center: 'Generally your tallest player',
+        swish: 'The sound of a shot that only hits the net',
+    },
+    hockey: {
+        puck: 'Hockey does not have a ball, it has a ...',
+        goalie: 'Defends the goal',
+        check: 'Legal hit of another player',
+        zamboni: 'Fixes the ice',
+        canada: 'The NHL plays in the US and ...'
+    }
+};
+
 let app = new Vue({
     el: '#scramble',
     data: {
@@ -8,43 +39,11 @@ let app = new Vue({
         player_name: '',
         selected_word: '',
         selected_sport: '',
-        words: {
-            baseball: {
-                batter: 'The person holding the bat',
-                pitcher: 'Stands on the mound',
-                umpire: 'Calls balls and strikes',
-                fastball: 'Pitch with the highest speed',
-                strike: 'A swing and a miss',
-            },
-            football: {
-                touchdown: 'Worth six points',
-                interception: 'A pass thrown to the defense',
-                safety: 'Sacked in your own end zone',
-                fumble: 'Dropped the ball',
-                yard: 'Unit of distance',
-            },
-            basketball: {
-                foul: 'Contact with another player',
-                dribble: 'Bounce the ball up and down',
-                basket: 'Where you shoot the ball',
-                center: 'Generally your tallest player',
-                swish: 'The sound of a shot that only hits the net',
-            },
-            hockey: {
-                puck: 'Hockey does not have a ball, it has a ...',
-                goalie: 'Defends the goal',
-                check: 'Legal hit of another player',
-                zamboni: 'Fixes the ice',
-                canada: 'The NHL plays in the US and ...'
-            }
-        },
+        sports: Object.keys(words),
+        words: words,
     },
 
     computed: {
-        sports: function () {
-            return Object.keys(this.words);
-        },
-
         message: function () {
             return this.correct_guess ? 'You got it! Nice work' : "Sorry, that's not correct. Please try again.";
         },
