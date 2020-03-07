f604a3413788877891d42726d3c7fdea4e9e077b
Andrew Bielecki <ambielecki@gmail.com>
2020-03-07 08:12:28 -0500

    week 6 :: title update

--- p1/templates/blackjack-message.php
+++ p1/templates/blackjack-message.php
@@ -1,3 +1,4 @@
+<?php // this was totally unnecessary ?>
 <script type="text/x-template" id="blackjack-message">
     <p :class="message_class" class="card-header-title">{{ message }}</p>
 </script>
