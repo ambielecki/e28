ad3342fc302b92eac2e340a9d1a44afcac3c213b
Andrew Bielecki <andrew.bielecki@healthcaresource.com>
2020-03-23 08:56:55 -0400

    week-7 :: small formatting fix

--- p1/templates/blackjack-table.php
+++ p1/templates/blackjack-table.php
@@ -2,7 +2,7 @@
     <div class="column is-full" id="dealer_area">
         <div class="columns is-mobile is-multiline">
             <div class="column is-full">
-                <h2 class="subtitle has-text-white">{{ owner + ': ' + owner_score}}</h2>
+                <h2 class="subtitle has-text-white">{{ owner + ': ' + owner_score }}</h2>
             </div>
 
             <blackjack-card
