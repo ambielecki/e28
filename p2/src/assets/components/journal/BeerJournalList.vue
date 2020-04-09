eb7bf63f82964fe0849c182c02e869b8310eaf6f
Andrew Bielecki <ambielecki@gmail.com>
2020-04-08 20:16:57 -0400

    week 9 :: fix env example

--- p2/src/assets/components/journal/BeerJournalList.vue
+++ p2/src/assets/components/journal/BeerJournalList.vue
@@ -6,8 +6,26 @@
     export default {
         data: function () {
             return {
-
+                beers: [],
             };
-        }
+        },
+        mounted: function () {
+            this.getBeers();
+        },
+        methods: {
+            getBeers(page = 1) {
+                window.Axios.get('/beer', {
+                    params: {
+                        page: page
+                    },
+                })
+                    .then(function (response) {
+                        console.log(response.data);
+                    })
+                    .catch(function (error) {
+                        console.log(error);
+                    });
+            },
+        },
     };
 </script>
