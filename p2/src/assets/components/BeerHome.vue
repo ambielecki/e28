967a042838b500905c789d504abd2b1595f30e9f
Andrew Bielecki <ambielecki@gmail.com>
2020-04-14 20:54:46 -0400

    week 10 :: home page from api (cms style)

--- p2/src/assets/components/BeerHome.vue
+++ p2/src/assets/components/BeerHome.vue
@@ -11,31 +11,45 @@
                 <p class="title">
                     Welcome to A Simple Homebrew Journal
                 </p>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus semper enim vitae felis pulvinar egestas. Vivamus dictum urna nec enim hendrerit suscipit. Maecenas vulputate semper lectus sit amet cursus. Morbi ut quam euismod, iaculis lorem et, semper sem. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris accumsan consectetur quam, iaculis placerat tortor tristique a. Sed blandit mauris odio, nec pharetra ligula molestie at. Proin mi enim, aliquam at quam id, faucibus tincidunt nisi. In dignissim quis massa eget volutpat.
-                </p>
-                <p>
-                    Nullam hendrerit pellentesque velit sed blandit. Nunc porta, quam in cursus eleifend, tortor lectus tempus eros, sit amet eleifend elit est eu orci. Quisque tellus diam, ultricies vitae lorem commodo, semper facilisis sem. Maecenas tortor dolor, malesuada a arcu nec, tempus imperdiet ipsum. Nam at neque tincidunt lorem dictum blandit. Aenean faucibus massa eget varius venenatis. Vivamus pulvinar vitae velit eu elementum. Donec quis ornare tortor.
-                </p>
-                <p>
-                    Nam sagittis neque nec libero viverra semper. Mauris consectetur sem et convallis vehicula. Sed eget metus vitae felis dapibus dignissim at non lectus. Nam id nibh arcu. Phasellus in sodales metus. Nunc sodales a augue auctor commodo. Donec maximus, magna sit amet auctor ornare, purus arcu mattis massa, eu pulvinar enim nibh sit amet erat. Nam at est ac velit condimentum mollis non eget magna.
-                </p>
-                <p>
-                    Vivamus eget nisl id ligula maximus commodo. Quisque euismod non neque vel porttitor. Maecenas interdum sed nibh eget rhoncus. Quisque risus quam, rutrum eu enim nec, condimentum faucibus ligula. Mauris elit purus, dapibus a tellus at, vestibulum dignissim tortor. Donec a ante magna. Praesent eu finibus ante. Donec justo eros, viverra id interdum id, laoreet vitae nibh. Sed fermentum, magna at viverra hendrerit, dui leo facilisis nisl, sit amet maximus lorem tellus ut massa. Fusce et pulvinar felis. Nullam et sem tincidunt, pretium massa quis, porta enim.
-                </p>
-                <p>
-                    Curabitur id lacus ut nisi placerat varius eu in nisl. Sed vitae nunc ligula. Donec et lacus non leo sollicitudin faucibus in at massa. Nam bibendum sed tortor eget tempus. Integer in convallis ligula. Integer eget faucibus urna. Integer semper leo non lorem finibus imperdiet. Interdum et malesuada fames ac ante ipsum primis in faucibus. Aliquam dui sem, viverra eget congue ac, finibus a diam. Nulla lacinia nec ipsum et rhoncus. Duis tristique feugiat erat, vitae placerat magna ultricies ac. Suspendisse maximus lacus gravida, rutrum ipsum id, feugiat velit. Vestibulum diam nulla, consectetur in velit hendrerit, condimentum porta nisi.
-                </p>
+                <div v-html="content" v-cloak></div>
             </div>
         </div>
     </div>
 </template>
 
 <script>
+    const Beer = require('../../Beer').default;
+    let beer = new Beer;
+
     export default {
         data: function () {
-            return {};
+            return {
+                content: '',
+            };
         },
+        mounted: function () {
+            window.Axios.get('/beer/home')
+                .then(response => {
+                    if (beer.validateResponse(response, 'page')) {
+                        if (Object.prototype.hasOwnProperty.call(response.data.data.page, 'content')) {
+                            this.content = response.data.data.page.content;
+                        }
+                    } else {
+                        this.content = 'There was a problem loading the home page, please try again.'
+                    }
+
+                })
+                .catch(error => {
+                    let error_messages = beer.formatErrorMessages(error);
+                    error_messages.forEach(error_message => {
+                        this.$emit('set-message', {
+                            time: 5,
+                            type: 'is-danger',
+                            message: error_message,
+                        });
+                    });
+                });
+        }
     };
 </script>
 
