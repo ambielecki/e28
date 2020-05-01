c049d44bbaf717fb1c52437975ef7dc677efaf9d
Andrew Bielecki <andrew.bielecki@healthcaresource.com>
2020-05-01 15:42:46 -0400

    week-12 :: start unit test

--- p3/tests/unit/BeerAlcoholCalculator.spec.js
+++ p3/tests/unit/BeerAlcoholCalculator.spec.js
@@ -4,15 +4,23 @@ import BeerAlcoholCalculator from '@/assets/components/tools/BeerAlcoholCalculat
 
 describe('BeerAlcoholCalculator.vue', () => {
     it('Calculates ABV Correctly', () => {
+        let starting_data = {
+            original_gravity: 1.055,
+            final_gravity: 1.008,
+        };
+
         const wrapper = shallowMount(BeerAlcoholCalculator, {
             data() {
-                return {
-                    original_gravity: 1.055,
-                    final_gravity: 1.008,
-                };
+                return starting_data;
             },
         });
 
+        // test correct value in text
         expect(wrapper.text()).to.include('6.2 %');
+
+        // test for correct return from computed
+        expect(BeerAlcoholCalculator.computed.abv.call(starting_data)).to.equal('6.2');
+        // test a negative case
+        expect(BeerAlcoholCalculator.computed.abv.call(starting_data)).to.not.equal('5.2');
     })
 })
