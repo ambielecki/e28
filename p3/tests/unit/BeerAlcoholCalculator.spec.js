c927ac8590e9fee745cf7578f6a0f92180aa51ab
Andrew Bielecki <andrew.bielecki@healthcaresource.com>
2020-05-01 16:05:10 -0400

    week-12 :: break into 2 separate tests

--- p3/tests/unit/BeerAlcoholCalculator.spec.js
+++ p3/tests/unit/BeerAlcoholCalculator.spec.js
@@ -3,12 +3,19 @@ import { shallowMount } from '@vue/test-utils'
 import BeerAlcoholCalculator from '@/assets/components/tools/BeerAlcoholCalculator.vue'
 
 describe('BeerAlcoholCalculator.vue', () => {
-    it('Calculates ABV Correctly', () => {
-        let starting_data = {
-            original_gravity: 1.055,
-            final_gravity: 1.008,
-        };
+    let starting_data = {
+        original_gravity: 1.055,
+        final_gravity: 1.008,
+    };
 
+    it('Calculates ABV Correctly', () => {
+        // test for correct return from computed
+        expect(BeerAlcoholCalculator.computed.abv.call(starting_data)).to.equal('6.2');
+        // test a negative case
+        expect(BeerAlcoholCalculator.computed.abv.call(starting_data)).to.not.equal('5.2');
+    });
+
+    it('Displays ABV Correctly', () => {
         const wrapper = shallowMount(BeerAlcoholCalculator, {
             data() {
                 return starting_data;
@@ -17,10 +24,7 @@ describe('BeerAlcoholCalculator.vue', () => {
 
         // test correct value in text
         expect(wrapper.text()).to.include('6.2 %');
-
-        // test for correct return from computed
-        expect(BeerAlcoholCalculator.computed.abv.call(starting_data)).to.equal('6.2');
-        // test a negative case
-        expect(BeerAlcoholCalculator.computed.abv.call(starting_data)).to.not.equal('5.2');
-    })
+        // test incorrect value is not displayed in text
+        expect(wrapper.text()).to.not.include('5.2 %');
+    });
 })
